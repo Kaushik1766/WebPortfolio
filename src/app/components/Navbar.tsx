15be0824e033ef6f08291b8e02effb5cbf51f5f5
Kaushik Saha <116095138+Kaushik1766@users.noreply.github.com>
2024-02-26 00:11:43 +0530

    WebPortfolio

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { SparklesCore } from "@/app/components/ui/sparkles";
+import { FloatingNav } from "@/app/components/ui/floatingNavbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+
+function Navbar() {
+    const navItems = [
+        {
+            name: "Home",
+            link: "/",
+            icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+        },
+        {
+            name: "About",
+            link: "/about",
+            icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+        },
+        {
+            name: "Contact",
+            link: "/contact",
+            icon: (
+                <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+            ),
+        },
+    ];
+    return (
+        <>
+            <FloatingNav navItems={navItems} />
+        </>
+    )
+}
+
+export default Navbar
